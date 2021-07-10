ef81028aaa94248de6829fab928028eeaf6fadab
NiutaR <69516148+NiutaR@users.noreply.github.com>
2021-07-10 13:30:30 +0300

    done

---
+++ .history/app_20210710131550.js
@@ -0,0 +1,7 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+
+let removeLastMessage = secretMessage.pop();
+console.log(secretMessage.length);
+
+let addWords = secretMessage.push('to', '')
---
+++ .history/app_20210710132155.js
@@ -0,0 +1,16 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+
+let removeLastMessage = secretMessage.pop();
+console.log(secretMessage.length);
+
+let addWords = secretMessage.push('to', 'Program');
+console.log(secretMessage);
+
+secretMessage[secretMessage.indexOf('easily')] = 'right';
+console.log(secretMessage);
+
+let removeFirst = secretMessage.shift();
+console.log(secretMessage);
+
+let a
